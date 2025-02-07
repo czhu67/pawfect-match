44599a8ed244aa318f5615e459d586a4c7f7f538
czhu67 <caitlin.zhu2010@gmail.com>
2025-02-06 20:41:39 -0500

    add Login page elements

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom";
+import { Button, TextField } from "@mui/material";
 
 export default function Login() {
     return (
         <div>
-            <Link to="/"></Link>
+            <TextField id="standard-basic" label="Name" variant="standard" />
+            <TextField id="standard-basic" label="Email" variant="standard" />
+            <Link to="/search">
+                <Button variant="contained">Log in</Button>
+            </Link>
         </div>
     );
 }
