5bdacad69908eee61732c6e3af282366e4aafa31
czhu67 <caitlin.zhu2010@gmail.com>
2025-02-07 15:06:33 -0500

    add fetch call

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,11 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
-
-const breedList = ['chihuahua', 'terrier']
+import { API_ENDPOINT } from "../App";
 
 export default function Search() {
     const [activeFilter, setActiveFilter] = useState("");
+    const [breedList, setBreedList] = useState([]);
+
+    const getBreeds = () => {getData();}
+    useEffect(getBreeds, []);
+
+    const getData = async () => {
+        const response = await fetch(`${API_ENDPOINT}/dogs/breeds`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            redirect: 'follow',
+            credentials: 'include',
+        });
+        if (response.ok) {
+            const breeds = await response.json();
+            setBreedList(breeds);
+        }
+    }
 
     const handleFilterChange = (e: { target: { value: string; }; }) => {
         setActiveFilter(e.target.value);
