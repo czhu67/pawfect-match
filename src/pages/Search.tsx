430f7dbe301a1d7001a7eb2b3a675b45638af3cc
czhu67 <caitlin.zhu2010@gmail.com>
2025-02-07 14:04:56 -0500

    setup dropdown filter

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+
+const breedList = ['chihuahua', 'terrier']
 
 export default function Search() {
+    const [activeFilter, setActiveFilter] = useState("");
+
+    const handleFilterChange = (e: { target: { value: string; }; }) => {
+        setActiveFilter(e.target.value);
+    }
+
     return (
-        <div>
+        <div className="page-container">
+            <FormControl>
+                <InputLabel>Breed</InputLabel>
+                <Select
+                    label="Breed"
+                    value={activeFilter}
+                    onChange={handleFilterChange}
+                >
+                    <MenuItem value=""></MenuItem>
+                    {breedList.map(breed => <MenuItem key={breed} value={breed}>{breed}</MenuItem>)}
+                </Select>
+            </FormControl>
             <Link to="/"></Link>
         </div>
     );
